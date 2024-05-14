b58d06fc137f885d383510d66e514ac04ccbd99a
Rolando0210 <rolanrdcv85@gmail.com>
2024-05-14 14:44:44 -0400

    Se agrego la URL del documento en google

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const AS_URL_BASE =
-  "https://script.google.com/macros/s/AKfycbzyd5-Cp-zpG1VUdUZfYL1AzOA1cHprhOf__507xqvs-Z6i869xi-v8PvGf6bFUoAtdlA/exec";
+  "https://script.google.com/macros/s/AKfycbxGx5CJu96BKwYVZrNW2KaoWkjBj9DIjgLtQ4yvhAxkDjyhI2EjshEx9YR_MeG4_8qI5Q/exec";
 
 var url = document.location.href;
 var k = url.substr(url.lastIndexOf("k=") + 2);
